273b054a818f218bb46c449f1b7555a7e1257de2
Ditjon Dafa <ditjon.dafa@outlook.com>
2026-06-08 17:12:21 +0200

    Added test to users header component.

--- app/users/components/header.tsx
+++ app/users/components/header.tsx
@@ -3,7 +3,7 @@ import styles from "@/app/ui/users/users.module.css";
 export default function Header() {
   return (
     <div className={styles.header}>
-      <h2> Users</h2>
+      <h2> Users list</h2>
     </div>
   );
 }
